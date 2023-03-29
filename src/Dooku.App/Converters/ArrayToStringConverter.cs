e3e4b75ff8599d7374ea20e9721df8343ee80289
Rob Hansen <robert.hansen@bglgroup.co.uk>
2023-03-29 18:34:01 +0100

    Added ArrayToStringConverter and bound to Button text

---
+++ src/Dooku.App/Converters/ArrayToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Dooku.App.Converters
+{
+    internal class ArrayToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var a = value as int[,];
+            string p = parameter.ToString();
+            var index1 = int.Parse(p.Split(",")[0]);
+            var index2 = int.Parse(p.Split(",")[1]);
+            return a[index1, index2];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Dooku.App/ViewModels/NumberViewModel.cs
+++ src/Dooku.App/ViewModels/NumberViewModel.cs
@@ -6,23 +6,40 @@ namespace Dooku.App.ViewModels
 {
     internal class NumberViewModel : ObservableObject
     {
-        public int[] TheNumber { get; set; }
+        public int[,] TheNumbers { get; set; }
+
+        public string ButtonHeight => "50";
+
+        public string ButtonWidth => "10";
+
+        public Thickness ButtonMargin => 10;
 
         public ICommand IncrementNumberCommand { get; }
 
         public NumberViewModel()
         {
             IncrementNumberCommand = new RelayCommand<string>(IncrementNumber);
-            if (TheNumber == null)
+            if (TheNumbers == null)
             {
-                TheNumber = new int[5];
+                TheNumbers = new int[9, 9];
             }
         }
 
         void IncrementNumber(string index)
         {
-            TheNumber[int.Parse(index)]++;
-            OnPropertyChanged(nameof(TheNumber));
+            var rowIndex = int.Parse(index[0].ToString());
+            var colIndex = int.Parse(index[1].ToString());
+            var currentValue = TheNumbers[rowIndex, colIndex];
+            if(currentValue == 9)
+            {
+                TheNumbers[rowIndex, colIndex] = 0;
+            }
+            else
+            {
+                TheNumbers[rowIndex, colIndex]++;
+            }
+
+            OnPropertyChanged(nameof(TheNumbers));
         }
     }
 }
